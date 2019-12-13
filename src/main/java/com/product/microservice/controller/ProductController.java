73b976c70fdf3d5ba70c2c80ff6f4708504136c6
Adrian Ayarde <aayarde@overactive.com>
2019-12-13 10:08:56 -0300

    Use Hystrix, Zuul gateway and filters, dynamic port.

--- src/main/java/com/product/microservice/controller/ProductController.java
+++ src/main/java/com/product/microservice/controller/ProductController.java
@@ -2,11 +2,14 @@ package com.product.microservice.controller;
 import com.product.microservice.model.entity.Product;
 import com.product.microservice.model.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class ProductController {
@@ -14,14 +17,35 @@ public class ProductController {
     @Autowired
     private IProductService productService;
 
+    @Autowired
+    private Environment environment;
+
+    @Value("${server.port}")
+    private Integer port;
+
     @GetMapping("/list")
     public List<Product> list(){
-        return productService.findAll();
+        return productService.findAll().stream().map(product -> {
+            //product.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+            product.setPort(port);
+            return product;
+        }).collect(Collectors.toList());
     }
 
     @GetMapping("/show/{id}")
-    public Product detail(@PathVariable Long id){
-        return productService.findById(id);
+    public Product detail(@PathVariable Long id) {
+        Product product = productService.findById(id);
+        boolean ok = false;
+        //product.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+        product.setPort(port);
+
+        /*try {
+            Thread.sleep(2000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+
+        return product;
     }
 
 }
